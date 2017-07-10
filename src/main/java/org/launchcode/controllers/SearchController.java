a7d8a8f64c400b05958730245a65aa092e75f314
cmp <cmpeters08@gmail.com>
2017-07-10 12:32:18 -0500

    Request handler passess nessassary information. the displays are working correctly.

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
@@ -24,15 +25,26 @@ public class SearchController {
     }
 
     //requestmapping is the route to the file to use for the handler
-    @RequestMapping(value = "/search")
+    @RequestMapping(value = "/results", method = RequestMethod.GET)
     public String results(Model model,
                           @RequestParam String searchType, @RequestParam String searchTerm){
-        ArrayList<HashMap<String, String>> searchResults = JobData.findByValue(searchTerm);
-        model.addAttribute("columns", ListController.columnChoices);
-        model.addAttribute("searchType", searchType);
-        model.addAttribute("searchTerm", searchTerm);
-        model.addAttribute("searchResults", searchResults);
-        return "/search/results";
+
+        if(searchType.equals("all")) {
+            ArrayList<HashMap<String, String>> jobs = JobData.findByValue(searchTerm);
+            model.addAttribute("columns", ListController.columnChoices);
+            model.addAttribute("searchType", searchType);
+            model.addAttribute("searchTerm", searchTerm);
+            model.addAttribute("jobs", jobs);
+        }
+        else{
+            ArrayList<HashMap<String,String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("columns", ListController.columnChoices);
+            model.addAttribute("searchType", searchType);
+            model.addAttribute("searchTerm", searchTerm);
+            model.addAttribute("jobs", jobs);
+        }
+        return "search";
+        //returns search.html
 
     }
 
